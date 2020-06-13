088157323f558b209d67dbbe8df3105de33d20ef
yejiang1015 <yejiang1015@gmail.com>
2020-06-13 12:15:32 +0800

    window mac

---
+++ views/components/WindowFrame/control/mac/icon/maximize.tsx
@@ -0,0 +1,37 @@
+/**
+ * 口 图标
+ */
+import { BaseWrapTypes, memoPropsAreEqual } from '@views/components/WindowFrame/utils';
+import React, { FC, memo, useState } from 'react';
+
+const Wrap: FC<BaseWrapTypes> = memo((props): React.ReactElement => {
+  const [width] = useState<number>(props.width || 15);
+  const [height] = useState<number>(props.height || 15);
+  const [color] = useState<string>(props.color || '#000');
+  return (
+    <>
+      <span onClick={props.onFunc}>
+        <svg className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7442" width={width} height={height}>
+          <path d="M832 832H192V192h640v640z m50-690H142v740h740V142z" p-id="7443" fill={color}></path>
+        </svg>
+      </span>
+      <style jsx>{`
+        span {
+          display: flex;
+          width: 12px;
+          height: 12px;
+          border-radius: 6px;
+          padding: 2px;
+          justify-content: center;
+          align-items: center;
+          background-color: green;
+        }
+        span:hover {
+          background-color: rgba(0, 0, 0, 0.08);
+        }
+      `}</style>
+    </>
+  );
+}, memoPropsAreEqual);
+
+export default Wrap;
---
+++ views/components/WindowFrame/control/mac/icon/mini.tsx
@@ -0,0 +1,34 @@
+import { BaseWrapTypes, memoPropsAreEqual } from '@views/components/WindowFrame/utils';
+import React, { FC, memo, useState } from 'react';
+
+const Wrap: FC<BaseWrapTypes> = memo((props): React.ReactElement => {
+  const [width] = useState<number>(props.width || 16);
+  const [height] = useState<number>(props.height || 16);
+  const [color] = useState<string>(props.color || '#000');
+  return (
+    <>
+      <span onClick={props.onFunc}>
+        <svg className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6887" width={width} height={height}>
+          <path d="M128 512h768a25.6 25.6 0 1 1 0 51.2h-768a25.6 25.6 0 1 1 0-51.2z" p-id="6888" fill={color}></path>
+        </svg>
+      </span>
+      <style jsx>{`
+        span {
+          display: flex;
+          width: 12px;
+          height: 12px;
+          border-radius: 6px;
+          padding: 2px;
+          justify-content: center;
+          align-items: center;
+          background-color: yellow;
+        }
+        span:hover {
+          background-color: rgba(0, 0, 0, 0.08);
+        }
+      `}</style>
+    </>
+  );
+}, memoPropsAreEqual);
+
+export default Wrap;
