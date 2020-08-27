2361f4abcdf09328f28faf2cb9f7969aeb970d9d
yejiang1015 <yejiang1015@gmail.com>
2020-08-27 11:03:14 +0800

    mac 系统托盘, 关闭非关闭

--- global/CreateBrowserWindow.ts
+++ global/CreateBrowserWindow.ts
@@ -2,7 +2,7 @@
  * @Msg nativeImage.createFromPath png 或者 jpg
  */
 
-import { BrowserWindow, IpcMainEvent, dialog, ipcMain, nativeImage } from 'electron';
+import { BrowserWindow, IpcMainEvent, app, dialog, ipcMain, nativeImage } from 'electron';
 const pkg = require('~/package.json');
 
 ipcMain.once('CreateBrowserWindow', (event: IpcMainEvent & { href: string }) => {
@@ -17,4 +17,31 @@ ipcMain.once('CreateBrowserWindow', (event: IpcMainEvent & { href: string }) => 
   const _BrowserWindow = new BrowserWindow(NewBrowserWindowOptions);
   _BrowserWindow.loadURL(event.href);
   Reflect.set(global, 'CreateBrowserWindow', _BrowserWindow);
+  /**
+   * @platform darwin Mac 平台关闭按钮不退出
+   * @Msg 创建窗口后监听窗口关闭，区分平台
+   * 关闭窗口事件，如果是quit退出，则清除全局主窗口缓存实例， 否则隐藏窗口
+   */
+  if (process.platform === 'darwin') {
+    Reflect.set(global, 'willQuitApp', false);
+    (Reflect.get(global, 'CreateBrowserWindow') as BrowserWindow).on('close', (e) => {
+      if (Reflect.get(global, 'willQuitApp')) {
+        Reflect.deleteProperty(global, 'CreateBrowserWindow');
+      } else {
+        e.preventDefault();
+        (Reflect.get(global, 'CreateBrowserWindow') as BrowserWindow).hide();
+      }
+    });
+    app.on('activate', () => (Reflect.get(global, 'CreateBrowserWindow') as BrowserWindow).show());
+
+    app.on('before-quit', () => Reflect.set(global, 'willQuitApp', true));
+  }
+  /**
+   * @platform win32 平台关闭按钮不退出，系统托盘
+   * @Msg 创建窗口后监听窗口关闭，区分平台
+   * 关闭窗口事件，如果是quit退出，则清除全局主窗口缓存实例， 否则隐藏窗口
+   */
+  if (process.platform === 'win32') {
+    /** TODO : */
+  }
 });
