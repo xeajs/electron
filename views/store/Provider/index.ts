0ca1e335cead3c3aef08f3f37ffbda6417b93913
yejiang <31330944+yejiang1015@users.noreply.github.com>
2020-09-17 13:52:31 +0800

    add mobx-react-devtools config

--- views/store/Provider/index.ts
+++ views/store/Provider/index.ts
@@ -6,6 +6,19 @@ import 'mobx-react-lite/batchingForReactDom';
 import { RouterStore } from 'mobx-react-router';
 import StoreList from '@views/store';
 import { configure } from 'mobx';
+import { configureDevtool } from 'mobx-react-devtools';
+
+/** ============== dev tools ============== */
+if (!$$.isPro()) {
+  configureDevtool({
+    /** 启用 log */
+    logEnabled: true,
+    /** 启用 updates */
+    updatesEnabled: true,
+    /** 启用 graph 图表 */
+    graphEnabled: true
+  });
+}
 
 /** ============== 严格模式 不允许在动作之外进行状态修改 ============== */
 configure({ enforceActions: 'observed' });
