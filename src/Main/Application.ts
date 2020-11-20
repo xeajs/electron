49691be84cf896c0991c20416180f3e66db9c12a
yejiang <31330944+yejiang1015@users.noreply.github.com>
2020-11-20 12:27:45 +0800

    取消使用 koa-compress。会导致服务响应 WaitingTTFB 时间过长

--- src/Main/Application.ts
+++ src/Main/Application.ts
@@ -5,14 +5,12 @@ import { Listen } from '@/Main/Core/Listen';
 import Middleware from '@/Main/Middlewares/Middleware';
 import Route from '@/Main/Route';
 import bodyparser from 'koa-bodyparser';
-import compress from 'koa-compress';
 import path from 'path';
 import serveStatic from 'koa-static';
 
 const app = new Koa();
 
 app.use(Middleware());
-app.use(compress());
 app.use(bodyparser());
 app.use(Route.routes());
 app.use(Route.allowedMethods());
