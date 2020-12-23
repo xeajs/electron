1db5f9a8b50f5afbfd982180fdb067337618fae9
yejiang <31330944+yejiang1015@users.noreply.github.com>
2020-12-23 18:45:48 +0800

    fix on error

--- src/Render/index.tsx
+++ src/Render/index.tsx
@@ -6,7 +6,3 @@ import ReactDOM from 'react-dom';
 import { remote } from 'electron';
 
 ReactDOM.render(App, window.document.getElementById('root'), remote.getCurrentWindow().show);
-
-window.onerror = (event: Event | string, source?: string, lineno?: number, colno?: number, error?: Error) => {
-  $$.log.error(error, error?.stack);
-};
