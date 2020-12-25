70ebaed74edb66cbce7d24042cf077742e741a3e
yejiang <31330944+yejiang1015@users.noreply.github.com>
2020-12-25 17:32:17 +0800

    重整 css 模块

--- src/Render/index.tsx
+++ src/Render/index.tsx
@@ -1,5 +1,5 @@
-import '@/Render/assets/css/style.css';
 import '@/Render/assets/css/antd/index.less';
+import '@/Render/assets/css/style/index.css';
 
 import App from '@/Render/route';
 import ReactDOM from 'react-dom';
