4c9fa045a42e3455dddbd8c949ce42c6402274a3
yejiang <31330944+yejiang1015@users.noreply.github.com>
2021-02-23 17:35:20 +0800

    router prefix

--- src/Main/Core/index.ts
+++ src/Main/Core/index.ts
@@ -1,15 +1,4 @@
-import Config from '~/config';
 export { Send } from './Send';
 export { Listen } from './Listen';
 export { PublishRender } from './PublishRender';
 export { Favicon } from './Favicon';
-
-/**
- * @param path restfull api path
- * @eg /hello  ==>
- */
-export const SetApiPrefix = (path: string) => {
-  path = path || '';
-  if (/^\//.test(path)) return Config.prefix + path;
-  return Config.prefix + '/' + path;
-};
--- src/Main/Route/index.ts
+++ src/Main/Route/index.ts
@@ -7,12 +7,12 @@
 import * as Hello from '@/Main/Controller/Hello';
 import * as Hello1 from '@/Main/Controller/Hello1';
 
+import Config from '~/config';
 import Router from 'koa-router';
-import { SetApiPrefix } from '@/Main/Core';
 
-const Route = new Router();
+const Route = new Router({ prefix: Config.prefix, strict: false, sensitive: true });
 
-Route.all(SetApiPrefix('/hello'), Hello.AllHello);
-Route.all(SetApiPrefix('/hello1'), Hello1.AllHello);
+Route.all('/hello', Hello.AllHello);
+Route.all('/hello1', Hello1.AllHello);
 
 export default Route;
