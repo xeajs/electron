365ebe618812aab418fd8afd9dca8f31064cee82
yejiang <yejiang1015@gmail.com>
2023-06-14 14:14:38 +0800

    feat: history api fallback

--- src/server/helper/deploy.ts
+++ src/server/helper/deploy.ts
@@ -1,7 +1,9 @@
 import Koa from 'koa'
 import serveStatic from 'koa-static'
+import historyApiFallback from 'koa2-connect-history-api-fallback'
 import path from 'node:path'
 
 export const deploy = async (app: Koa) => {
+  app.use(historyApiFallback())
   app.use(serveStatic(path.resolve(__dirname, '../'), { gzip: true }))
 }
